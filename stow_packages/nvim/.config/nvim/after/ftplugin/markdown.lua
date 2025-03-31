d48cafdc9a9836a9c620330c6a2856d6c2793567
Jasper Zanjani <jasper.zanjani@gmail.com>
2025-03-31 13:08:23 -0500

    md keybindings

--- stow_packages/nvim/.config/nvim/after/ftplugin/markdown.lua
+++ stow_packages/nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -12,3 +12,7 @@ vim.keymap.set('v', '<leader>b', 'S*gv2lS*', { remap = true })
 -- Build mkdocs-material
 vim.keymap.set('n', '<leader><S-CR>', '!make')
 vim.keymap.set('n', '<F10>', '<cmd>!make<CR>', {})
+
+vim.keymap.set('n', '<leader>I', 'o```<Enter><Enter>```<Esc>k$')
+vim.keymap.set('n', '<leader>i', 'o--8<-- "includes/"<Esc>i')
+vim.keymap.set('n', '<leader>g', 'a<div class="grid cards" markdown><enter><enter><enter><enter></div><Esc>2-a-<tab>')
